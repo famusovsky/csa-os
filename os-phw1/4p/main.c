418b1e903c793d8c7449020f8e1afdbef7e49963
Alexey Stepanov <st3p-by-step@ya.ru>
2023-03-17 12:24:03 +0300

    I have understood the task not right -- fix

--- os-phw1/4p/main.c
+++ os-phw1/4p/main.c
@@ -14,18 +14,17 @@
 #define BUFFER_SIZE 5000
 
 int main(int argc, char *argv[]) {
-  // Проверка наличия двух аргументов командной строки: входного файла и выходного файла
+  // Проверка наличия двух аргументов командной строки: входного файла и
+  // выходного файла
   if (argc != 3) {
     printf("Usage: %s input_file output_file\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  // Объявление двух буферов для хранения данных
-  char buffer1[BUFFER_SIZE], buffer2[BUFFER_SIZE];
-
   // Создание двух каналов для обмена данными между процессами
   int pipe1[2], pipe2[2];
-  pid_t pid1, pid2;
+  // Создание трёх процессов
+  pid_t pid1, pid2, pid3;
 
   if (pipe(pipe1) < 0 || pipe(pipe2) < 0) { // Проверка ошибок при создании каналов
     perror("pipe");
@@ -38,15 +37,18 @@ int main(int argc, char *argv[]) {
   } else if (pid1 == 0) {
     // Дочерний процесс 1 -- чтение данных из входного файла
     close(pipe1[0]); // Закрытие неиспользуемого конца канала pipe1
-    // Открытие входного файла и чтение данных в буфер buffer1
+    // Объявление буфера
+    char buffer[BUFFER_SIZE];
+
+    // Открытие входного файла и чтение данных в буфер buffer
     int input_fd = open(argv[1], O_RDONLY);
-    ssize_t read_size = read(input_fd, buffer1, BUFFER_SIZE);
+    ssize_t read_size = read(input_fd, buffer, BUFFER_SIZE);
     if (read_size < 0) {
       perror("read");
       exit(EXIT_FAILURE);
     }
     // Запись данных из буфера buffer1 в канал pipe1
-    write(pipe1[1], buffer1, read_size);
+    write(pipe1[1], buffer, read_size);
     close(input_fd); // Закрытие файлового дескриптора входного файла
     close(pipe1[1]); // Закрытие конца канала, используемого для записи
     exit(EXIT_SUCCESS); // Завершение дочернего процесса
@@ -58,11 +60,12 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   } else if (pid2 == 0) {
     // Дочерний процесс 2 -- обработка данных
-    close(pipe1[1]); // Закрытие конца канала, используемого для записи
+    close(pipe1[1]); // Закрытие конца канала pipe1, используемого для записи
     close(pipe2[0]); // Закрытие неиспользуемого конца канала pipe2
-
+    // Объявление буфера
+    char buffer[BUFFER_SIZE];
     // Чтение данных из канала pipe1 в буфер buffer2
-    ssize_t read_size = read(pipe1[0], buffer2, BUFFER_SIZE);
+    ssize_t read_size = read(pipe1[0], buffer, BUFFER_SIZE);
     if (read_size < 0) {
       perror("read");
       exit(EXIT_FAILURE);
@@ -74,24 +77,24 @@ int main(int argc, char *argv[]) {
     char result[read_size]; // Буфер для хранения результата
     for (int i = 0; i < read_size; i++) {
       // При окончании второй строки прекращаем обработку данных
-      if (buffer2[i] == '\n' && current_lines_count == 0) {
+      if (buffer[i] == '\n' && current_lines_count == 0) {
         current_lines_count++;
         continue;
-      } else if (buffer2[i] == '\n' && current_lines_count > 0) {
+      } else if (buffer[i] == '\n' && current_lines_count > 0) {
         break;
       }
 
       // Поиск текущего символа среди уже пройденных символов
       int is_char_found = 0;
       for (int j = 0; j < i; j++) {
-        if (buffer2[i] == buffer2[j]) {
+        if (buffer[i] == buffer[j]) {
           is_char_found = 1;
           break;
         }
       }
       // Если символ встречается впервые, добавляем его в выходную строку
       if (is_char_found == 0) {
-        result[index] = buffer2[i];
+        result[index] = buffer[i];
         index++;
       }
     }
@@ -102,25 +105,36 @@ int main(int argc, char *argv[]) {
     exit(EXIT_SUCCESS);
   }
 
-  // Родительский процесс -- запись данных в выходной файл
+  // Создание третьего процесса
+  if ((pid3 = fork()) < 0) { // Проверка ошибок при создании процесса
+    perror("fork");
+    exit(EXIT_FAILURE);
+  } else if (pid3 == 0) {
+    close(pipe2[1]); // Закрытие конца канала pipe2, используемого для записи
+    // Объявление буфера
+    char buffer[BUFFER_SIZE];
+    // Открытие выходного файла
+    int output_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    ssize_t read_size =
+        read(pipe2[0], buffer, BUFFER_SIZE); // Чтение данных из канала pipe2 в буфер buffer1
+    if (read_size < 0) { // Проверка ошибок при чтении данных из канала
+      perror("read");
+      exit(EXIT_FAILURE);
+    }
+    write(output_fd, buffer, read_size); // Запись данных из буфера buffer1 в выходной файл
+    close(pipe2[0]);
+    close(output_fd);
+    exit(EXIT_SUCCESS);
+  }
+
+  waitpid(pid1, NULL, 0); //
+  waitpid(pid2, NULL, 0); // Ожидание завершения всех процессов
+  waitpid(pid3, NULL, 0); //
+
   close(pipe1[0]); //
   close(pipe1[1]); // Закрытие концов каналов, используемых для чтения и записи
+  close(pipe2[0]); //
   close(pipe2[1]); //
 
-  // Открытие выходного файла
-  int output_fd =
-      open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-  ssize_t read_size =
-      read(pipe2[0], buffer1,
-           BUFFER_SIZE); // Чтение данных из канала pipe2 в буфер buffer1
-  if (read_size < 0) { // Проверка ошибок при чтении данных из канала
-    perror("read");
-    exit(EXIT_FAILURE);
-  }
-  write(output_fd, buffer1,
-        read_size); // Запись данных из буфера buffer1 в выходной файл
-  close(pipe2[0]);
-  close(output_fd);
-
-  return 0;
+  return EXIT_SUCCESS;
 }
--- os-phw1/5p/main.c
+++ os-phw1/5p/main.c
@@ -9,9 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
 
 #define BUFFER_SIZE 5000
 
@@ -22,55 +22,63 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  // Объявление двух буферов для хранения данных
-  char buffer1[BUFFER_SIZE], buffer2[BUFFER_SIZE];
+  // Создание трёх процессов
+  pid_t pid1, pid2, pid3;
 
-  // Создание двух каналов для обмена данными между процессами
-  pid_t pid1, pid2;
-
+  unlink("fifo1"); // На случай, если каналы уже существуют
+  unlink("fifo2"); // На случай, если каналы уже существуют
   // Проверка ошибок при создании каналов
-  if (mkfifo("fifo1", 0666) == -1 || mkfifo("fifo2", 0666) == -1) {
+  if (mkfifo("fifo1", 0666) == -1) {
+    perror("mkfifo");
+    exit(EXIT_FAILURE);
+  }
+  if (mkfifo("fifo2", 0666) == -1) {
     perror("mkfifo");
     exit(EXIT_FAILURE);
   }
 
   if ((pid1 = fork()) < 0) {
-    perror("fork");
+    perror("fork1");
     exit(EXIT_FAILURE);
   } else if (pid1 == 0) {
     // Дочерний процесс 1 -- чтение данных из входного файла
-    // Открытие входного файла и чтение данных в буфер buffer1
+    // Объявление буфера
+    char buffer[BUFFER_SIZE];
+    // Открытие входного файла и канала fifo1
     int input_fd = open(argv[1], O_RDONLY);
     int fd1 = open("fifo1", O_WRONLY);
 
-    // Чтение данных из входного файла в буфер buffer1
-    ssize_t read_size = read(input_fd, buffer1, BUFFER_SIZE);
+    // Чтение данных из входного файла в буфер
+    ssize_t read_size = read(input_fd, buffer, BUFFER_SIZE);
     if (read_size < 0) {
-      perror("read");
+      perror("read1");
       exit(EXIT_FAILURE);
     }
-    // Запись данных из буфера buffer1 в канал pipe1
-    write(fd1, buffer1, read_size);
+
+    // Запись данных из буфера buffer в канал fifo1
+    write(fd1, buffer, read_size);
     close(input_fd); // Закрытие файлового дескриптора входного файла
     close(fd1); // Закрытие файлового дескриптора канала fifo1
-    unlink("fifo1");    // Удаление канала
+    // unlink("fifo1");    // Удаление канала
     exit(EXIT_SUCCESS); // Завершение дочернего процесса
   }
 
   // Создание второго процесса
   if ((pid2 = fork()) < 0) { // Проверка ошибок при создании процесса
-    perror("fork");
+    perror("fork2");
     exit(EXIT_FAILURE);
   } else if (pid2 == 0) {
     // Дочерний процесс 2 -- обработка данных
+    // Объявление буфера
+    char buffer[BUFFER_SIZE];
     // Открываем именованные каналы fifo1 для чтения и fifo2 для записи
     int fd1 = open("fifo1", O_RDONLY);
     int fd2 = open("fifo2", O_WRONLY);
 
-    // Чтение данных из канала fifo1 в буфер buffer2
-    ssize_t read_size = read(fd1, buffer2, BUFFER_SIZE);
+    // Чтение данных из канала fifo1 в буфер
+    ssize_t read_size = read(fd1, buffer, BUFFER_SIZE);
     if (read_size < 0) {
-      perror("read");
+      perror("read2");
       exit(EXIT_FAILURE);
     }
 
@@ -80,24 +88,24 @@ int main(int argc, char *argv[]) {
     char result[read_size]; // Буфер для хранения результата
     for (int i = 0; i < read_size; i++) {
       // При окончании второй строки прекращаем обработку данных
-      if (buffer2[i] == '\n' && current_lines_count == 0) {
+      if (buffer[i] == '\n' && current_lines_count == 0) {
         current_lines_count++;
         continue;
-      } else if (buffer2[i] == '\n' && current_lines_count > 0) {
+      } else if (buffer[i] == '\n' && current_lines_count > 0) {
         break;
       }
 
       // Поиск текущего символа среди уже пройденных символов
       int is_char_found = 0;
       for (int j = 0; j < i; j++) {
-        if (buffer2[i] == buffer2[j]) {
+        if (buffer[i] == buffer[j]) {
           is_char_found = 1;
           break;
         }
       }
       // Если символ встречается впервые, добавляем его в выходную строку
       if (is_char_found == 0) {
-        result[index] = buffer2[i];
+        result[index] = buffer[i];
         index++;
       }
     }
@@ -105,22 +113,38 @@ int main(int argc, char *argv[]) {
     write(fd2, result, index); // Запись данных из буфера result в канал fifo2
     close(fd1); // Закрытие файловых дескрипторов канала fifo1
     close(fd2); // Закрытие файловых дескрипторов канала fifo2
-    unlink("fifo1"); // Удаление канала
-    unlink("fifo2"); // Удаление канала
-    exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS); // Завершение дочернего процесса
   }
 
-  // Родительский процесс -- запись данных в выходной файл
-  int fd2 = open("fifo2", O_RDONLY); // Открытие канала fifo2 для чтения
-  int output_fd = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR); // Открытие выходного файла
-  ssize_t read_size = read(fd2, buffer1, BUFFER_SIZE); // Чтение данных из канала fifo2 в буфер buffer1
-  if (read_size < 0) { // Проверка ошибок при чтении данных из канала
-    perror("read");
+  // Создание третьего процесса
+  if ((pid3 = fork()) < 0) { // Проверка ошибок при создании процесса
+    perror("fork3");
     exit(EXIT_FAILURE);
+  } else if (pid3 == 0) {
+    // Объявление буфера
+    char buffer[BUFFER_SIZE];
+    // Открываем именованный канал fifo2 для чтения
+    int fd2 = open("fifo2", O_RDONLY);
+    // Открытие выходного файла
+    int output_fd = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
+
+    ssize_t read_size = read(fd2, buffer, BUFFER_SIZE); // Чтение данных из канала fifo2 в буфер buffer1
+    if (read_size < 0) { // Проверка ошибок при чтении данных из канала
+      perror("read3");
+      exit(EXIT_FAILURE);
+    }
+    write(output_fd, buffer, read_size); // Запись данных из буфера buffer1 в выходной файл
+    close(output_fd); // Закрытие файлового дескриптора выходного файла
+    close(fd2); // Закрытие файловых дескрипторов канала fifo2
+    exit(EXIT_SUCCESS); // Завершение дочернего процесса
   }
-  write(output_fd, buffer1, read_size); // Запись данных из буфера buffer1 в выходной файл
-  close(fd2); // Закрытие файловых дескрипторов канала fifo2
-  close(output_fd); // Закрытие файловых дескрипторов выходного файла
-  unlink("fifo2"); // Удаление канала
-  return 0;
+
+  waitpid(pid1, NULL, 0); //
+  waitpid(pid2, NULL, 0); // Ожидание завершения всех дочерних процессов
+  waitpid(pid3, NULL, 0); //
+
+  unlink("fifo1"); // Удаление канала fifo1
+  unlink("fifo2"); // Удаление канала fifo2
+
+  return EXIT_SUCCESS;
 }
