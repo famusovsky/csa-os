9d6e3886b6b2dc643892f47c07d52588aadc8d9b
Alexey Stepanov <st3p-by-step@ya.ru>
2023-04-16 20:18:51 +0300

    OS PHW2 Update

--- os-phw2/4p/main.c
+++ os-phw2/4p/main.c
@@ -16,12 +16,10 @@
 #include <time.h>
 #include <unistd.h>
 
-const int MSGS_COUNT = 10;
-const int BUFSIZE = 256;
-#define SEM_KEY 1918
+const int ROOMS_CNT = 3;
 
-int getSemaphoreSet(int cnt_sems) {
-  int sem_id = semget(SEM_KEY, cnt_sems, IPC_CREAT | 0666);
+int getSemaphoreSet(int cnt_sems, int sem_key) {
+  int sem_id = semget(sem_key, cnt_sems, IPC_CREAT | 0666);
   if (sem_id < 0) {
     perror("Error creating semaphore set: ");
     exit(EXIT_FAILURE);
@@ -41,24 +39,8 @@ int getSemaphoreSet(int cnt_sems) {
 void eraseSemaphore(int sem_id) {
   printf("Removing semaphore set with id: %d\n", sem_id);
 
-  if (semctl(sem_id, IPC_RMID, NULL) < 0) {
-    perror("Error removing semaphore: ");
-    exit(EXIT_FAILURE);
-  }
-}
-
-void runOpForAllSemaphoresInSet(int sem_id, int sem_op, int sem_flg) {
-  int cnt_sems = semctl(sem_id, 0, GETNCNT);
-  struct sembuf sops[cnt_sems];
-
-  for (int i = 0; i < cnt_sems; ++i) {
-    sops[i].sem_num = i;
-    sops[i].sem_op = sem_op;
-    sops[i].sem_flg = sem_flg;
-  }
-
-  if (semop(sem_id, sops, 1) < 0) {
-    perror("Error running semoperations: ");
+  if (semctl(sem_id, 0, IPC_RMID) < 0) {
+    perror("Error removing semaphore set: ");
     exit(EXIT_FAILURE);
   }
 }
@@ -77,58 +59,106 @@ void runOp(int sem_id, int sem_num, int sem_op, int sem_flg) {
 }
 
 // TODO: safe exit on SIGINT and end of the work
-void hotel_process(int hotel_sem_id, int visitors_sem_id) {
-  runOpForAllSemaphoresInSet(hotel_sem_id, 1, 0);
+void hotel_process(int visitors_sem_id, int rooms_sem_id) {
+  printf("Hotel is opened.\n");
 
-  int sem_val = semctl(visitors_sem_id, 0, GETVAL);
-  if (sem_val <= 0) {
-    // TODO: end the work
+  while (1) {
+    printf("Waiting for visitors...\n");
+    runOp(visitors_sem_id, 1, 0, SEM_UNDO);
+
+    int vsem_val = semctl(visitors_sem_id, 0, GETVAL);
+    printf("Count of still waiting visitors: %d\n", vsem_val);
+
+    if (vsem_val == 0) {
+      break;
+    }
+
+    printf("Checking rooms...\n");
+    for (int i = 0; i < ROOMS_CNT; ++i) {
+      int room_val = semctl(rooms_sem_id, i, GETVAL);
+      if (room_val > 0) {
+        runOp(rooms_sem_id, i, -1, 0);
+        printf("Room %d will be free in %d days\n", i + 1, room_val);
+      } else {
+        printf("Room %d is free\n", i + 1);
+      }
+    }
+
+    printf("All rooms are checked\n");
+    runOp(visitors_sem_id, 1, vsem_val, 0);
   }
 }
 
 // TODO: safe exit on SIGINT
-void visitor_process(int hotel_sem_id, int visitors_sem_id) {
-  int nsems = semctl(hotel_sem_id, 0, GETNCNT);
+void visitor_process(int visitors_sem_id, int rooms_sem_id, int num) {
+  short is_done = 0;
+  srand(time(NULL) + num);
+  printf("Visitor %d is created\n", num);
 
-  for (int i = 0; i < nsems; ++i) {
-    int sem_val = semctl(hotel_sem_id, i, GETVAL);
+  while (1) {
+    printf("Visitor %d is waiting for the hotel\'s opening\n", num);
+    while (semctl(visitors_sem_id, 1, GETVAL) == 0) {
+      sleep(1);
+    }
 
-    if (sem_val > 0) {
-      runOp(hotel_sem_id, i, rand() % 7 + 1, 0);
-      // TODO: stay in the hotel and leave
-      printf("Visitor %d is staying in the room %d", getpid(), i);
-      exit(EXIT_SUCCESS);
+    printf("Visitor %d is looking for a room\n", num);
+    for (int i = 0; i < ROOMS_CNT; ++i) {
+      if (semctl(rooms_sem_id, i, GETVAL) == 0) {
+        runOp(rooms_sem_id, i, rand() % 7 + 1, 0);
+        runOp(visitors_sem_id, 0, -1, 0);
+        is_done = 1;
+        printf("Visitor %d is staying in the room %d for %d days\n", num, i + 1,
+               semctl(rooms_sem_id, i, GETVAL));
+        break;
+      } else {
+        printf("Visitor %d finds out that room %d is busy for %d days\n", num,
+               i + 1, semctl(rooms_sem_id, i, GETVAL));
+      }
+      printf("Visitor %d stops checking room %d\n", num, i + 1);
+    }
+
+    printf("Visitor %d\'s day is over\n", num);
+    runOp(visitors_sem_id, 1, -1, 0);
+    if (is_done) {
+      printf("Visitor %d\'s room search is over\n", num);
+      break;
     }
   }
 }
 
 int main(int argc, char **argv) {
-  int hotel_sem_id;
+  srand(time(NULL));
+
   int visitors_sem_id;
-  int cnt_rooms = 30;
-  int cnt_visitors = 10;
+  int rooms_sem_id;
+  int visitors_cnt = 10;
 
   if (argc > 1) {
-    cnt_visitors = atoi(argv[1]);
+    visitors_cnt = atoi(argv[1]);
   }
 
-  hotel_sem_id = getSemaphoreSet(cnt_rooms);
-  visitors_sem_id = getSemaphoreSet(1);
-  runOp(visitors_sem_id, 0, cnt_visitors, 0);
+  visitors_sem_id = getSemaphoreSet(2, rand() % 10000);
+  rooms_sem_id = getSemaphoreSet(ROOMS_CNT, rand() % 10000);
 
-  srand(time(NULL));
+  runOp(visitors_sem_id, 0, visitors_cnt, 0);
+
   // TODO: safe exit on SIGINT
-  for (int i = 0; i < cnt_visitors; ++i) {
+  for (int i = 0; i < visitors_cnt; ++i) {
     pid_t pid;
     if ((pid = fork()) < 0) {
       perror("Error creating visitor process\n");
       exit(EXIT_FAILURE);
     } else if (pid == 0) {
-      visitor_process(hotel_sem_id, visitors_sem_id);
+      visitor_process(visitors_sem_id, rooms_sem_id, i + 1);
+      exit(EXIT_SUCCESS);
     }
   }
 
-  hotel_process(hotel_sem_id, visitors_sem_id);
+  hotel_process(visitors_sem_id, rooms_sem_id);
 
+  eraseSemaphore(rooms_sem_id);
+  eraseSemaphore(visitors_sem_id);
+
+  printf("Hotel is closed.\n");
   return 0;
 }
